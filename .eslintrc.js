8204ac01247a3d8c73d6106c6dd89664a560c789
junpeko5 <junpeko5@gmail.com>
2020-01-11 17:47:10 +0900

    tsxに変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,51 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: [
+    "@typescript-eslint",
+    "jest",
+    "prettier",
+    "prefer-arrow",
+    "react",
+    "react-hooks"
+  ],
+  root: true,
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", "jsx", ".ts", ".tsx"]
+      }
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
+    react: {
+      version: "detect"
     }
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "always",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ],
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }]
+  }
 };
